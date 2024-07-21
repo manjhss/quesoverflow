b798a55befc42c4b0d6eb7ad6b043e35c8a6813f
Surendra <manjhisurendra08@gmail.com>
2024-07-22 03:09:46 +0530

    Appwrite and nextjs setup

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+const env = {
+  appwrite: {
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+    apiKey: process.env.APPWRITE_API_KEY,
+  },
+};
+
+export default env;
